499cbebbdadae6c0b496726c534d1d44fdd7c908
Dmitry Nazarov <mail@nazarov.tech>
2018-01-04 19:13:28 +0300

    Add stubs for input data

---
+++ run.py
@@ -0,0 +1,9 @@
+def generate_big_sample(lines_amount):
+    path = 'data/'
+    filename = 'big_sample.txt'
+    pass
+
+
+if __name__ == "__main__":
+    pass
+    # todo read and write unique IPs to output.txt
