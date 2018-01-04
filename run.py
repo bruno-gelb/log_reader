7139a45791c28ce02e2ffa78d31341ba590eb86e
Dmitry Nazarov <mail@nazarov.tech>
2018-01-04 19:35:04 +0300

    Add big file generation

--- run.py
+++ run.py
@@ -1,15 +1,30 @@
 # todo measure processing time (timeit?.. consider multiprocessing)
 # todo handle real log format (probably via regex for IPs)
 import os
+from random import randrange
+
+
+def generate_ip():
+    # codingwithcody.com/2010/07/03/generate-random-ip-with-python/
+    not_valid = [10, 127, 169, 172, 192]
+
+    first = randrange(1, 256)
+    while first in not_valid:
+        first = randrange(1, 256)
+
+    ip = ".".join([str(first), str(randrange(1, 256)),
+                   str(randrange(1, 256)), str(randrange(1, 256))])
+    return ip
 
 
 def generate_big_sample(lines_amount):
-    path = 'data/'
-    filename = 'big_sample.txt'
-    os.path.join()
-    # with open()
+    filename = os.path.join(os.getcwd(), 'data', 'big_sample.txt')
+    with open(filename, 'w') as f:
+        for i in range(lines_amount):
+            # todo write in bulk instead. Speed is okay tho since it's one-time generation
+            print(generate_ip(), file=f)
 
 
 if __name__ == "__main__":
-    pass
+    generate_big_sample(10 ** 7)
     # todo read and write unique IPs to output.txt
