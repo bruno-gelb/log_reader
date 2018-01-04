c9ecfb1332be24ae927b57e850f5ce0608e1ae99
Dmitry Nazarov <mail@nazarov.tech>
2018-01-04 19:42:04 +0300

    Add initial structure for reading

--- run.py
+++ run.py
@@ -2,11 +2,18 @@
 # todo handle real log format (probably via regex for IPs)
 
 
-def gather_unique_ips(filename):
-    pass
-    # todo read and write unique IPs to output.txt
+def gather_unique_ips(input, output):
+    unique_ips = set()  # todo maybe not the best choice, think about it
+
+    with open(input, 'r') as f:
+        pass
+        # todo read unique_ips
+
+    with open(output, 'w') as f:
+        pass  # todo write unique_ips to output.txt
 
 
 if __name__ == "__main__":
     # generate_big_sample(10 ** 7)
-    gather_unique_ips('data/small_sample.txt')
+    gather_unique_ips(input='data/small_sample.txt',
+                      output='output.txt')
