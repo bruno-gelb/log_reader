0e2eb2684affa77a29cf6e7ff773e0f40f51c120
Dmitry Nazarov <mail@nazarov.tech>
2018-01-04 19:48:57 +0300

    Get rid of shadowing builtins

--- run.py
+++ run.py
@@ -2,18 +2,18 @@
 # todo handle real log format (probably via regex for IPs)
 
 
-def gather_unique_ips(input, output):
+def gather_unique_ips(input_file, output_file):
     unique_ips = set()
 
-    with open(input, 'r') as f:
+    with open(input_file, 'r') as f:
         for line in f:
             unique_ips.add(line.strip())
 
-    with open(output, 'w') as f:
+    with open(output_file, 'w') as f:
         f.writelines('%s\n' % ip for ip in unique_ips)
 
 
 if __name__ == "__main__":
     # generate_big_sample(10 ** 7)
-    gather_unique_ips(input='data/small_sample.txt',
-                      output='output.txt')
+    gather_unique_ips(input_file='data/small_sample.txt',
+                      output_file='output.txt')
