373a693f068b04a41833a2810cd837a567d59bac
Dmitry Nazarov <mail@nazarov.tech>
2018-01-04 20:45:21 +0300

    Add multiprocessing (not tested)

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # todo handle real logfile format (probably via regex for IPs)
 
 import os
+from multiprocessing import Process
 from timeit import default_timer as timer
 
 from utils import sizeof_fmt
@@ -9,11 +10,11 @@ from utils import sizeof_fmt
 PROCESSES_NUMBER = 2  # later set it up to 8
 
 
-def gather_unique_ips(input_file, output_file):
+def gather_unique_ips(input_file, output_file, offset):
     unique_ips = set()
 
     with open(input_file, 'r') as f:
-        for line in f:
+        for line in f[::offset]:
             unique_ips.add(line.strip())
 
     with open(output_file, 'w') as f:
@@ -23,13 +24,18 @@ def gather_unique_ips(input_file, output_file):
 if __name__ == "__main__":
     # generate_big_sample(10 ** 7)
 
-    input_file = 'data/big_sample.txt'
+    input_file = 'data/small_sample.txt'
     output_file = 'output.txt'
 
+    jobs = []
     start = timer()
-    gather_unique_ips(input_file, output_file)
-
+    for i in range(PROCESSES_NUMBER):
+        p = Process(target=gather_unique_ips,
+                    args=(input_file, output_file, i,))
+        jobs.append(p)
+        p.start()
     end = timer()
+
     print('Text file with size {0} was handled in {1:.2f} sec'.format(
         sizeof_fmt(os.path.getsize(input_file)), end - start)
     )
