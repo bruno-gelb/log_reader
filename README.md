fd346dca544c9130ea386db88efcd9dfc6eb30ea
Dmitry Nazarov <mail@nazarov.tech>
2018-01-04 19:22:12 +0300

    Elaborate on readme

---
+++ README.md
@@ -0,0 +1,13 @@
+### purpose
+
+TODO
+
+### stack
+
+python3
+
+
+### how to install & use
+```shell
+python3 run.py
+```
--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+# todo measure processing time (timeit?.. consider multiprocessing)
+# todo handle real log format (probably via regex for IPs)
+import os
+
+
 def generate_big_sample(lines_amount):
     path = 'data/'
     filename = 'big_sample.txt'
-    pass
+    os.path.join()
+    # with open()
 
 
 if __name__ == "__main__":
