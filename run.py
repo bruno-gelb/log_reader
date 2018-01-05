5df5147df98a3561c667710d758a6da1dd249c7e
Dmitry Nazarov <mail@nazarov.tech>
2018-01-05 08:30:48 +0300

    Add initial offset logic to worker code

--- run.py
+++ run.py
@@ -1,35 +1,54 @@
-# todo handle real logfile format (probably via regex for IPs)
-
 import os
 from multiprocessing import Process
 from timeit import default_timer as timer
 
 from utils import sizeof_fmt
 
-PROCESSES_NUMBER = 2  # later set it up to 8
+PROCESSES_NUMBER = 3  # later set it up to 4-8
+
+
+# 2-worker case:
+# 1, 3, 5, 7, 9
+# 2, 4, 6, 8
+
+
+# 3-worker case:
+# 1, 4, 7
+# 2, 5, 8
+# 3, 6, 9
 
 
 def gather_unique_ips(input_file, output_file, worker_number):
     start = timer()
     unique_ips = set()
 
+    offset = worker_number - 1
+
     # todo add offset work here (based on worker_number)
     with open(input_file, 'r') as f:
-        for line in f:
-            unique_ips.add(line.strip())
+        for i, line in enumerate(f):
+            line_number = i + 1
+            if line_number <= offset:
+                continue
+
+            # todo handle line skipping on offset
+
+            ip = line.strip()  # todo handle real logfile format (probably via regex for IPs)
+            print('Worker #{}, line {}'.format(worker_number, ip))
+            unique_ips.add(ip)
 
     with open(output_file, 'w') as f:
         f.writelines('%s\n' % ip for ip in unique_ips)
     end = timer()
 
     # todo find out the better way to measure time in multiprocessing context =/
-    print('Worker #{} finished in {} sec'.format(worker_number, end - start))
+    print('Worker #{} finished in {:.2f} sec'.format(worker_number, end - start))
 
 
 if __name__ == "__main__":
     # generate_big_sample(10 ** 7)
 
-    input_file = 'data/big_sample.txt'
+    input_file = 'data/small_sample_2.txt'
     output_file = 'output.txt'
 
     print('Text file ({0}) handling started..'.format(
@@ -40,6 +59,6 @@ if __name__ == "__main__":
 
     for i in range(PROCESSES_NUMBER):
         p = Process(target=gather_unique_ips,
-                    args=(input_file, output_file, i,))
+                    args=(input_file, output_file, i + 1,))
         jobs.append(p)
         p.start()
